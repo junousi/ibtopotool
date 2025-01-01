5e46f047753e3a5f350a24c4c29cb15d4f51a810
Jukka Nousiainen <jukka.nousiainen@iki.fi>
2025-01-01 12:41:57 +0200

    Add tests
    Add some test topologies found from various sources, accompanied
    with a test script for validation.

---
+++ tests/run_tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set +e
+
+for TOPO in *.topo; do
+
+  # If e.g. pydot postprocessing would be incorporated,
+  # it would make sense to handle extensions case-by-case.
+  # But for now, use a bulk extension ".out" for all cases.
+  OUTFILE=${TOPO%.*topo}.out
+
+  # Opportunistically assign the first found switch as a single treeify root
+  TREEIFY=t.tmp
+  grep -E '^Switch' ${TOPO} | head -1 | awk '{print $3}' | sed 's/"//g' > ${TREEIFY}
+
+  # Execute tests
+  CMD="python3 ../src/ibtopotool.py -o ${OUTFILE} ${TOPO}";                                $CMD && echo "Success: ${CMD}" || echo "Failure: ${CMD}"
+  CMD="python3 ../src/ibtopotool.py -s -o ${OUTFILE} ${TOPO}";                             $CMD && echo "Success: ${CMD}" || echo "Failure: ${CMD}"
+  CMD="python3 ../src/ibtopotool.py -s --shortlabels -o ${OUTFILE} ${TOPO}";               $CMD && echo "Success: ${CMD}" || echo "Failure: ${CMD}"
+  CMD="python3 ../src/ibtopotool.py -t ${TREEIFY} -o ${OUTFILE} ${TOPO}";                  $CMD && echo "Success: ${CMD}" || echo "Failure: ${CMD}"
+  CMD="python3 ../src/ibtopotool.py -t ${TREEIFY} --shortlabels -o ${OUTFILE} ${TOPO}";    $CMD && echo "Success: ${CMD}" || echo "Failure: ${CMD}"
+  CMD="python3 ../src/ibtopotool.py -s -t ${TREEIFY} -o ${OUTFILE} ${TOPO}";               $CMD && echo "Success: ${CMD}" || echo "Failure: ${CMD}"
+  CMD="python3 ../src/ibtopotool.py -s -t ${TREEIFY} --shortlabels -o ${OUTFILE} ${TOPO}"; $CMD && echo "Success: ${CMD}" || echo "Failure: ${CMD}"
+  CMD="python3 ../src/ibtopotool.py --slurm -o ${OUTFILE} ${TOPO}";                        $CMD && echo "Success: ${CMD}" || echo "Failure: ${CMD}"
+  CMD="python3 ../src/ibtopotool.py --slurm -t ${TREEIFY} -o ${OUTFILE} ${TOPO}";          $CMD && echo "Success: ${CMD}" || echo "Failure: ${CMD}"
+  CMD="python3 ../src/ibtopotool.py --slurm -s -o ${OUTFILE} ${TOPO}";                     $CMD && echo "Success: ${CMD}" || echo "Failure: ${CMD}"
+  CMD="python3 ../src/ibtopotool.py --slurm -s -t ${TREEIFY} -o ${OUTFILE} ${TOPO}";       $CMD && echo "Success: ${CMD}" || echo "Failure: ${CMD}"
+
+done
+
+#Cleanup
+rm ${TREEIFY}
+if stat *.out 1>/dev/null 2>/dev/null; then
+  rm *.out
+fi
