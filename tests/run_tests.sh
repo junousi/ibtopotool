e1c17ed0b75c8591e83ecd6d416ebdbfc2e95938
Jukka Nousiainen <jukka.nousiainen@iki.fi>
2025-01-14 21:36:33 +0200

    Simplify
    In the end there was no need for treeify file to be a variable.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+
 set -e
 
-TREEIFY=t.tmp
 cleanup() {
   if stat *.out 1>/dev/null 2>/dev/null; then rm *.out; fi
-  if stat ${TREEIFY} 1>/dev/null 2>/dev/null; then rm ${TREEIFY}; fi
+  if stat t.tmp 1>/dev/null 2>/dev/null; then rm t.tmp; fi
 }
 printfail_cleanup() {
   echo "Failure: ${CMD}"
@@ -12,32 +12,28 @@ printfail_cleanup() {
 }
 trap printfail_cleanup ERR
 
-## These could be added at some point.
-#for EXAMPLE in net net.1 net.2sw2path net.2sw2path4hca net.2sw2path4hca2port; do
-#  curl "https://raw.githubusercontent.com/linux-rdma/ibsim/refs/heads/master/net-examples/${EXAMPLE}" -o ${EXAMPLE}.topo
-#done
 for TOPO in *.topo; do
 
   # If e.g. pydot postprocessing would be incorporated,
   # it would make sense to handle extensions case-by-case.
-  # But for now, use a bulk extension ".out" for all cases.
+  # But for now, use a bulk extension ".out" for all output cases.
   OUTFILE=${TOPO%.*topo}.out
 
   # Opportunistically assign the first found switch as a single treeify root
-  grep -E '^Switch' ${TOPO} | head -1 | awk '{print $3}' | sed 's/"//g' > ${TREEIFY}
+  grep -E '^Switch' ${TOPO} | head -1 | awk '{print $3}' | sed 's/"//g' > t.tmp
 
   CMD_ARRAY=(
     "python3 ../src/ibtopotool.py -o ${OUTFILE} ${TOPO}"
     "python3 ../src/ibtopotool.py -s -o ${OUTFILE} ${TOPO}"
     "python3 ../src/ibtopotool.py -s --shortlabels -o ${OUTFILE} ${TOPO}"
-    "python3 ../src/ibtopotool.py -t ${TREEIFY} -o ${OUTFILE} ${TOPO}"
-    "python3 ../src/ibtopotool.py -t ${TREEIFY} --shortlabels -o ${OUTFILE} ${TOPO}"
-    "python3 ../src/ibtopotool.py -s -t ${TREEIFY} -o ${OUTFILE} ${TOPO}"
-    "python3 ../src/ibtopotool.py -s -t ${TREEIFY} --shortlabels -o ${OUTFILE} ${TOPO}"
+    "python3 ../src/ibtopotool.py -t t.tmp -o ${OUTFILE} ${TOPO}"
+    "python3 ../src/ibtopotool.py -t t.tmp --shortlabels -o ${OUTFILE} ${TOPO}"
+    "python3 ../src/ibtopotool.py -s -t t.tmp -o ${OUTFILE} ${TOPO}"
+    "python3 ../src/ibtopotool.py -s -t t.tmp --shortlabels -o ${OUTFILE} ${TOPO}"
     "python3 ../src/ibtopotool.py --slurm -o ${OUTFILE} ${TOPO}"
-    "python3 ../src/ibtopotool.py --slurm -t ${TREEIFY} -o ${OUTFILE} ${TOPO}"
+    "python3 ../src/ibtopotool.py --slurm -t t.tmp -o ${OUTFILE} ${TOPO}"
     "python3 ../src/ibtopotool.py --slurm -s -o ${OUTFILE} ${TOPO}"
-    "python3 ../src/ibtopotool.py --slurm -s -t ${TREEIFY} -o ${OUTFILE} ${TOPO}"
+    "python3 ../src/ibtopotool.py --slurm -s -t t.tmp -o ${OUTFILE} ${TOPO}"
   )
 
   # Execute tests
@@ -50,3 +46,9 @@ for TOPO in *.topo; do
 
   cleanup
 done
+
+
+## These could be added at some point, but that will require some python side parsing adjustments
+#for EXAMPLE in net net.1 net.2sw2path net.2sw2path4hca net.2sw2path4hca2port; do
+#  curl "https://raw.githubusercontent.com/linux-rdma/ibsim/refs/heads/master/net-examples/${EXAMPLE}" -o ${EXAMPLE}.topo
+#done
